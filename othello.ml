067ac8f66b13e57953501d4dd41b8c747f1cd5b2
Jeremi Dupin <jeremi.dupin@gmail.com>
2014-10-10 22:20:48 +0200

    amelioration de la configuration

--- othello.ml
+++ othello.ml
@@ -1,3 +1,12 @@
+type config = {
+    nbcols  : int;
+    nbrows  : int
+} ;;
+type coord = {
+    x   : int;
+    y   : int
+} ;;
 type cell = White | Black | Empty
-type board = (cell array) array
-type coord = {x:int; y:int};;
+type board = cell array array
+
+let default_config = { nbcols=8; nbrows=10} ;;
