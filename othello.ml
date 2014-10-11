f859ec291c12f70f044d50e69383e830df90999b
Jeremi Dupin <jeremi.dupin@gmail.com>
2014-10-11 16:59:29 +0200

    Placement du carré central

--- othello.ml
+++ othello.ml
@@ -9,4 +9,27 @@ type coord = {
 type cell = White | Black | Empty
 type board = cell array array
 
-let default_config = { nbcols=8; nbrows=10} ;;
+(* Configuration par défaut *)
+let default_config = { nbcols=8; nbrows=8 } ;;
+
+(* Iterateur sur l'ensemble des cases du plateau *)
+let iter_on_cell cf f =
+    for i=0 to cf.nbcols-1 do for j=0 to cf.nbrows-1 do f (i,j) done done
+;;
+
+(* Construction du plateau *)
+let make_board =
+    Array.init default_config.nbcols (fun y -> Array.init default_config.nbrows
+    (fun x -> Empty)
+    )
+;;
+
+(* Placement du carré central *)
+let init_board =
+    let board = make_board in
+    board.((Array.length board)/2-1).((Array.length board)/2-1) <- White;
+    board.((Array.length board)/2).((Array.length board)/2) <- White;
+    board.((Array.length board)/2).((Array.length board)/2-1) <- Black;
+    board.((Array.length board)/2-1).((Array.length board)/2) <- Black;
+    board
+;;
