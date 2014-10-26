3b7f2455a4e21b39a24ea1db7dd24f5c5621aa8d
Jeremi Dupin <jeremi.dupin@gmail.com>
2014-10-26 18:12:28 +0100

    Jeux fonctionnel

--- othello.ml
+++ othello.ml
@@ -1,23 +1,25 @@
 (********************************
  ****** PARTIE CONTROLLEUR ******
  ********************************)
+type cell = White | Black | Empty ;;
 type config = {
     nbcols  : int;
-    nbrows  : int
+    nbrows  : int;
+    faction : cell;
+    bg_r    : int;
+    bg_g    : int;
+    bg_b    : int;
+    cell_size : int
 } ;;
 type coord = {
     x   : int;
     y   : int
 } ;;
-type cell = White | Black | Empty ;;
 type board = cell array array ;;
 
 (* Configuration par défaut *)
-let default_config = { nbcols=8; nbrows=8 } ;;
-let cell_size = ref 25
-let bg_r = ref 199
-let bg_g = ref 222
-let bg_b = ref 109
+let default_config = { nbcols=8; nbrows=8; faction=Black; bg_r=199; bg_g=222; bg_b=109;
+cell_size=25 } ;;
 
 (* Directions possibles pour la capture *)
 let directions = [
@@ -113,38 +115,66 @@ done;
 !finished
 ;;
 
+(* Le tour du joueur *)
+let player_turn board =
+    let rec player_turn_rec () =
+        let st = Graphics.wait_next_event[Graphics.Button_down] in
+        let x = (st.Graphics.mouse_x / default_config.cell_size)
+        and y = (st.Graphics.mouse_y / default_config.cell_size) in
+        if (playable_cell board default_config.faction x y) then
+            play_cell board default_config.faction x y
+        else
+            player_turn_rec ()
+        in player_turn_rec ()
+;;
+
+(* Le tour de l'ordinateur *)
+let rec ia_turn board =
+ let x = Random.int (Array.length board) in
+ let y = Random.int (Array.length board.(0)) in
+ match board.(x).(y) with
+ | Empty ->
+         if (playable_cell board White x y) then
+             play_cell board White x y
+         else
+             ia_turn board
+ | _ -> ia_turn board
+;;
 
 (******************************
  ****** PARTIE GRAPHIQUE ******
  ******************************)
+
+
 (* Affiche une case *)
 let display_cell board x y =
-    Graphics.set_color (Graphics.rgb !bg_r !bg_g !bg_b);
+    Graphics.set_color (Graphics.rgb default_config.bg_r default_config.bg_g
+    default_config.bg_b);
     Graphics.fill_rect
-        (y * !cell_size + 1)
-        (x * !cell_size + 1)
-        (!cell_size - 2)
-        (!cell_size - 2);
+        (y * default_config.cell_size + 1)
+        (x * default_config.cell_size + 1)
+        (default_config.cell_size - 2)
+        (default_config.cell_size - 2);
     Graphics.set_color Graphics.black;
     Graphics.draw_rect
-        (y * !cell_size)
-        (x * !cell_size)
-        (y + !cell_size - y)
-        (x + !cell_size - x);
+        (y * default_config.cell_size)
+        (x * default_config.cell_size)
+        (y + default_config.cell_size - y)
+        (x + default_config.cell_size - x);
     Graphics.set_color (match board.(y).(x) with
                         | Black -> Graphics.black
                         | _ -> Graphics.white);
         if ( not ( board.(y).(x) = Empty)) then
         (
             Graphics.fill_circle
-                (y * !cell_size + !cell_size/2)
-                (x * !cell_size + !cell_size/2)
-                (!cell_size / 2 - 2);
+                (y * default_config.cell_size + default_config.cell_size/2)
+                (x * default_config.cell_size + default_config.cell_size/2)
+                (default_config.cell_size / 2 - 2);
             Graphics.set_color Graphics.black;
             Graphics.draw_circle
-                (y * !cell_size + !cell_size/2)
-                (x * !cell_size + !cell_size/2)
-                (!cell_size / 2 -2);
+                (y * default_config.cell_size + default_config.cell_size/2)
+                (x * default_config.cell_size + default_config.cell_size/2)
+                (default_config.cell_size / 2 -2);
         )
 ;;
 
@@ -153,8 +183,8 @@ let display_board board =
     Graphics.open_graph
     (Printf.sprintf
     " %dx%d"
-    (!cell_size * Array.length board.(0)+1)
-    (21 + !cell_size * Array.length board.(0)));
+    (default_config.cell_size * Array.length board.(0)+1)
+    (21 + default_config.cell_size * Array.length board.(0)));
     for i=0 to Array.length board-1 do
         for j=0 to Array.length board.(i)-1 do
             display_cell board i j;
@@ -178,10 +208,24 @@ let display_message message =
 (***********************
  ****** LANCEMENT ******
  ***********************)
-let game() =
+let continue() =
+    let st = (Graphics.wait_next_event [Graphics.Button_down]) in
+    st.Graphics.button <> true
+;;
+let game ()=
     let board = ref make_board in
     display_board !board;
-
+    while not (is_finished !board) do
+        display_message "Black to play...";
+        player_turn !board;
+        display_board !board;
+        display_message "White to play...";
+        ia_turn !board;
+        display_board !board;
+    done;
+    while continue() do
+        ()
+    done;
     Graphics.close_graph
 ;;
 
